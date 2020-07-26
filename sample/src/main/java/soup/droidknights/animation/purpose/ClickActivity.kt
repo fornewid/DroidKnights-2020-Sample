855c53671fd6d5c8ebcbdac53501005832dea411
SOUP <soupyong@gmail.com>
2020-07-27 01:46:14 +0900

    update

--- sample/src/main/java/soup/droidknights/animation/purpose/ClickActivity.kt
+++ sample/src/main/java/soup/droidknights/animation/purpose/ClickActivity.kt
@@ -1,7 +1,9 @@
 package soup.droidknights.animation.purpose
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import soup.droidknights.animation.R
 import soup.droidknights.animation.hideNavBar
 
@@ -11,5 +13,9 @@ class ClickActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.purpose_click)
         hideNavBar()
+
+        findViewById<View>(R.id.button).setOnClickListener {
+            it.isVisible = false
+        }
     }
 }
